cd5f5e8e1912e565a696de2ae02911c03526adc8
ingrididk <165039166+ingrididk@users.noreply.github.com>
2024-04-18 13:33:32 +0200

    bug dix

--- elevator.py
+++ elevator.py
@@ -4,7 +4,7 @@ import random as r
 class Skyscraper:
     def __init__(self, num_floors, num_elevators):
         self.top = num_floors
-        self.elevators = [Elevator(5) for i in range(num_elevators)]
+        self.elevators = [MajorityElevator(5) for i in range(num_elevators)]
         #Antar att floorstates är en lista av tasks som väntar där
         self.floor_state = [[] for i in range(num_floors)]  
 
@@ -25,13 +25,20 @@ class Skyscraper:
                 elv.waiting_on_floor -= 1
             elif choice == 2:
                 #släpper av passagerare
-                elv.check_for_arraival()
-
+                elv.check_for_arrival()
                 #passagerare går på
-                for task in self.floor_state[elv.current_floor]:
+                m = 0
+                for i in range(len(self.floor_state[elv.current_floor])):
                     if elv.max_load > len(elv.tasks):
-                        elv.tasks.append(task)
-                        self.floor_state[elv.current_floor].remove(task)
+                        elv.tasks.append(self.floor_state[elv.current_floor][i])
+                    else:
+                        m= i
+                        break
+                if m != 0:
+                    self.floor_state[elv.current_floor] = self.floor_state[elv.current_floor][m+1:]
+                else: self.floor_state[elv.current_floor] = []
+
+                
 
                 elv.waiting_on_floor = self.wait_cost
             elif choice == 0:
@@ -61,17 +68,75 @@ class Elevator:
         self.waiting_on_floor = 0
     
     #bestämmer om hissen ska gå upp(return 0), ner(return 1) eller släppa av/på passagerare(return 2)
-    def make_choice(self,information:list)->int:
+    def make_choice(self,information:list)->int: 
         return 2
 
-    def check_for_arraival(self):
-        for task in self.tasks:
-            if task.dest == self.current_floor:
-                self.totaltime += task.time**2
-                self.tasks.remove(task)
+    def check_for_arrival(self):
+        l = []
+        for i in range(len(self.tasks)):
+            if self.tasks[i].dest == self.current_floor:
+                self.totaltime += self.tasks[i].time**2
+                l.append(i)
+        l.reverse()
+        for i in l:
+            self.tasks.remove(self.tasks[i])
 
 
  #   def finish_task 
+                
+# Hissen som går till den våning där det finns flest människor som vill på/av
+class MajorityElevator(Elevator):
+
+    # samma konstruktor, med ett extra attribut
+    def __init__(self, max_load):
+        super().__init__(max_load)
+        self.destination = (False, None) # om den har bestämt sig för en destination, ska den åka dit tills den är klar
+        # self.passengers_entering = 0 # håller koll på passagerare som håller på att stiga upp
+
+    def choice_to_return(self, travel_to):
+        # 2 stannar den, 1 går den ner, 2 går den upp
+            if self.current_floor == travel_to:
+                return 2
+            elif self.current_floor > travel_to:
+                return 1
+            elif self.current_floor < travel_to: 
+                return 0
+
+    # får in floor_states som information, det är en lista med listor
+    def make_choice(self, information: list) -> int:
+        
+        # # om personer håller på att gå på, fortsätter den låta dem göra det
+        # if self.passengers_entering > 0:
+        #     self.passengers_entering -= 1
+        #     return 2
+        
+        if self.destination[0]:
+            if self.destination[1] == self.current_floor:
+                # # passengers är -1 för en får gå av första gången
+                # passengers_entering = len(information[self.current_floor]) - 1 
+                self.destination = (False, None)
+                return 2
+            else:
+                return self.choice_to_return(self.destination[1])
+
+        # om den inte har en destination, beräknar den det ist
+        # skapar en lista, med hur många som väntar på varje våning
+        floor_list = list() 
+        floors = len(information)    
+        for i in range(floors):
+            floor_list.append(len(information[i]))
+        for task in self.tasks:
+            floor_list[task.dest] += 1
+
+        # ger den våning dit flest vill
+        # har två samma tar den den våningen som är närmast bottenplan
+        travel_to = floor_list.index(max(floor_list))
+        self.destination = (True, travel_to)
+        return self.choice_to_return(travel_to)
+
+        
+
+        #return floor_list
 
 
 class Task:
@@ -80,17 +145,33 @@ class Task:
         self.dest = dest 
         self.time = 0 
 
+    # för debugging
+    def __repr__(self):
+        return f'Task(start: {self.floor}, dest: {self.dest}, time: {self.time})'
+
 #för debugging
 if __name__ == "__main__":
     skyscrape = Skyscraper(4,1)
-    skyscrape.floor_state[0].append(Task(0,2))
-    print(skyscrape.floor_state,skyscrape.elevators[0].tasks)
-    skyscrape.time_step()
-    print(skyscrape.floor_state,skyscrape.elevators[0].tasks)
-    skyscrape.elevators[0].current_floor = 2
-    skyscrape.elevators[0].waiting_on_floor = 0
-    skyscrape.time_step()
-    print(skyscrape.floor_state,skyscrape.elevators[0].tasks)
+    
+    #skyscrape.floor_state[1].append(Task(1,2))
+    skyscrape.floor_state[2].append(Task(2,3))
+    skyscrape.floor_state[2].append(Task(2,3))
+
+    for _ in range(10):
+        skyscrape.time_step()
+        print(f'floor states: {skyscrape.floor_state} \n tasks: {skyscrape.elevators[0].tasks} \n floor: {skyscrape.elevators[0].current_floor} \n =====')
+        
+   
+
+    # skyscrape = Skyscraper(4,1)
+    # skyscrape.floor_state[0].append(Task(0,2))
+    # print(skyscrape.floor_state,skyscrape.elevators[0].tasks)
+    # skyscrape.time_step()
+    # print(skyscrape.floor_state,skyscrape.elevators[0].tasks)
+    # skyscrape.elevators[0].current_floor = 2
+    # skyscrape.elevators[0].waiting_on_floor = 0
+    # skyscrape.time_step()
+    # print(skyscrape.floor_state,skyscrape.elevators[0].tasks)
 
 
 
